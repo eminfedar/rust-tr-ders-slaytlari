30d5a059441dea142c051fd7fdf72c3aad7f86ab
Arda Altıntaş <73823004+ardanyan@users.noreply.github.com>
2024-01-31 09:24:40 +0300

    week-1
    week-1

--- exercises/week-1/src/main.rs
+++ exercises/week-1/src/main.rs
@@ -5,10 +5,10 @@
 // fn multiply_pi(num: f32) -> f32
 // - fonksiyonu verilen sayıyı Pi sayısı ile çarpıp geri döndürecek.
 
-use std::f32::consts;
+
 
 fn hello(name: &str) -> String {
-    format!("Hello {name}!")
+    format!("Hello, {}!", name)
 }
 
 fn double_if_even(num: i32) -> i32 {
@@ -20,14 +20,15 @@ fn double_if_even(num: i32) -> i32 {
 }
 
 fn multiply_pi(num: f32) -> f32 {
-    num * consts::PI
+    num * std::f32::consts::PI
 }
 
+
 fn main() {
-    println!("{}", hello("Emin"));
+    println!("{}", hello("Arda"));
 
     println!("{}", double_if_even(10));
     println!("{}", double_if_even(5));
 
-    println!("{}", multiply_pi(2.0));
+    println!("{}", multiply_pi(3.0));
 }
